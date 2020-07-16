94933b21c4cacedbc83a9b79adc1db8bfbae9427
Sherren Soesanto <sherrensoesanto@gmail.com>
2020-07-16 22:16:57 +0800

    updated the following: - countdown timer and calculate score - leaderboard sections

--- app/src/main/java/iss/workshop/flipMe/GameActivity.java
+++ app/src/main/java/iss/workshop/flipMe/GameActivity.java
@@ -3,10 +3,13 @@ package iss.workshop.flipMe;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +18,19 @@ import android.widget.TextView;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
+
+import static android.content.SharedPreferences.*;
 
 
 public class GameActivity extends AppCompatActivity
     implements View.OnClickListener{
 
-    int secs = 0;
+    long remainder;
+    CountDownTimer countDownTimer;
+    //int secs = 0;
     int score = 0;
     EditText playerName;
     AlertDialog popUpBox;
@@ -49,6 +58,8 @@ public class GameActivity extends AppCompatActivity
         gridView.setAdapter(imageAdapter);
         gridView.setVerticalScrollBarEnabled(false);
 
+        popUpDialogue();
+
         startTimer();
     }
 
@@ -57,11 +68,22 @@ public class GameActivity extends AppCompatActivity
         int id = view.getId();
 
         if(id==R.id.submitBtn){
+
             SharedPreferences pref =getSharedPreferences("players", MODE_PRIVATE);
             SharedPreferences.Editor editor = pref.edit();
-            editor.putString("name", playerName.toString());
-            editor.putInt("score", score);
+
+            int i = 0;
+            while (true){
+                if(!pref.contains("name" + i)){
+                    break;
+                }
+                i++;
+            }
+
+            editor.putString("name" + i, playerName.toString());
+            editor.putInt("score" + i, score);
             editor.commit();
+
             popUpBox.dismiss();
 
             //route user to leader board listing
@@ -73,24 +95,33 @@ public class GameActivity extends AppCompatActivity
 
     private void startTimer(){
         final TextView timer = findViewById(R.id.timer);
-        final Handler timerHandler = new Handler();
-        timerHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                int minutes =(secs % 3600)/60;
-                int seconds =secs % 60;
+//        final Handler timerHandler = new Handler();
+//        timerHandler.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                int minutes =(secs % 3600)/60;
+//                int seconds =secs % 60;
+//                timer.setText(String.format("%d:%2d", minutes,seconds));
+//                secs++;
+//                timerHandler.postDelayed(this, 500);
+//            }
+//        });
+        countDownTimer = new CountDownTimer(60000, 1000){
+            public void onTick(long millisUntilFinished){
+                int minutes = (int) millisUntilFinished/6000;
+                int seconds = (int) (millisUntilFinished/6000)/1000;
                 timer.setText(String.format("%d:%2d", minutes,seconds));
-                secs++;
-                timerHandler.postDelayed(this, 500);
+                remainder = millisUntilFinished;
             }
-        });
+            public void onFinish(){
+                stopGame();
+
+            }
+        }.start();
     }
 
     private void totalScore(){
-        score = (60 - secs) * 10;
-        if (score < 0){
-            score = 0;
-        }
+        score = (int) remainder /100;
     }
 
     public void popUpDialogue() {
@@ -129,4 +160,12 @@ public class GameActivity extends AppCompatActivity
         popUpBox.show();
 
     }
+
+    void stopGame(){
+        countDownTimer.cancel();
+    }
+
+
+
+
 }
