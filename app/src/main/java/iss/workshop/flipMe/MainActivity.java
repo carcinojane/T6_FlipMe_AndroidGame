8f36b08390c9062f965e162c5e2280b180298c2c
Sherren Soesanto <sherrensoesanto@gmail.com>
2020-07-16 10:53:19 +0800

    updated the GameActivity: - added timer - added scoring (WIP)

--- app/src/main/java/iss/workshop/flipMe/MainActivity.java
+++ app/src/main/java/iss/workshop/flipMe/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,11 +16,14 @@ public class MainActivity extends AppCompatActivity
 implements View.OnClickListener {
     String url;
     ArrayList<ImageDAO> images;
+    int secs = 0;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //get UI elements
         Button btnStart = findViewById(R.id.btnStart);
@@ -36,6 +41,8 @@ implements View.OnClickListener {
         int id = view.getId();
         if(id==R.id.btnStart) {
             //get images bitmap
+            //start timer
+            startTimer();
 
 
 
@@ -43,6 +50,28 @@ implements View.OnClickListener {
             Intent intent = new Intent(this, WebViewActivity.class);
             startActivity(intent);
 
+        }
+    }
+
+    private void startTimer(){
+        final TextView timer = findViewById(R.id.timer);
+        final Handler timerHandler = new Handler();
+        timerHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                int minutes =(secs % 3600)/60;
+                int seconds =secs % 60;
+                timer.setText(String.format("%d:%2d", minutes,seconds));
+                secs++;
+                timerHandler.postDelayed(this, 500);
+            }
+        });
+    }
+
+    private void totalScore(){
+        score = (60 - secs) * 10;
+        if (score < 0){
+            score = 0;
         }
     }
 }
