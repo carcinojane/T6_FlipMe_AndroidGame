8f21aaac2171e3c5a5560aa23d28e40cd5feaddd
Lydia <lydia.hojy@gmail.com>
2020-07-18 10:24:35 +0800

    changed mp4. Added back button to activity_video and cleaned up formatting

--- app/src/main/java/iss/workshop/flipMe/VideoActivity.java
+++ app/src/main/java/iss/workshop/flipMe/VideoActivity.java
@@ -2,14 +2,18 @@ package iss.workshop.flipMe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 import android.widget.MediaController;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity
+    implements View.OnClickListener{
 
     protected VideoView videoView;
     protected MediaController mc;
@@ -19,12 +23,29 @@ public class VideoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 |View.SYSTEM_UI_FLAG_FULLSCREEN);
         playVideo();
+
+        Button backBtn = findViewById(R.id.btnBack);
+        if(backBtn!=null){
+            backBtn.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+        if (id == R.id.btnBack){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     protected void playVideo(){
